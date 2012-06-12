c563d096aac4afbd844daedda4bb6518b269616c
Thomas Boltze <thomas.boltze@gmail.com>
2012-06-12 19:10:26 +0800

    Fixed Zone namespace issue

--- app/controllers/spree/admin/shipping_methods_controller_decorator.rb
+++ app/controllers/spree/admin/shipping_methods_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::Admin::ShippingMethodsController.class_eval do
 
   # overriding to return an array of Calculator objects instead of a Set
   def load_data
-    @available_zones = Zone.find :all, :order => :name
+    @available_zones = Spree::Zone.find :all, :order => :name
     @calculators = []
     ShippingMethod.calculators.each {|calc|
       @calculators << eval(calc.name).new
