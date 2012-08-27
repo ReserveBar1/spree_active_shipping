f017bd06ca7bfc2257114f1b6a470f2acf9b87ee
Thomas Boltze <thomas.boltze@gmail.com>
2012-08-27 14:28:23 +0800

    Added service type for int'l

--- app/models/spree/calculator/fedex/international_priority_saturday_delivery.rb
+++ app/models/spree/calculator/fedex/international_priority_saturday_delivery.rb
@@ -2,6 +2,11 @@ module Spree
   class Calculator::Fedex::InternationalPrioritySaturdayDelivery < Calculator::Fedex::Base
     def self.description
       I18n.t("fedex.intl_priority_saturday_delivery")
-    end  
+    end
+    
+    def self.service_type
+      "INTERNATIONAL_PRIORITY_SATURDAY_DELIVERY"
+    end
+      
   end
 end
